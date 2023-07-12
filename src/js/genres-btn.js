d8a60c8cd48a5d2a1137c78642deba33d03e9325
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-07-12 11:23:12 +0300

    added footer and genres-btn, custom-toggle

---
+++ src/js/genres-btn.js
@@ -0,0 +1,28 @@
+import { dataGenres } from './test-genres';
+
+const dropdown = document.querySelector('.dropdown');
+const items = document.querySelector('.items');
+
+export { items };
+
+dropdown.addEventListener('click', onButtonClick);
+function btnGenres() {
+  dataGenres.genres.unshift({ id: 'all', name: 'ALL' });
+  const liArr = dataGenres.genres
+    .map(
+      (element, index) =>
+        `<a href="#" style="--i:${index + 1}" id="${element.id}">
+           <span></span>${element.name}
+         </a>`
+    )
+    .join('');
+
+  items.insertAdjacentHTML('beforeend', liArr);
+}
+
+btnGenres();
+
+function onButtonClick(evt) {
+  evt.preventDefault();
+  dropdown.classList.toggle('active');
+}
